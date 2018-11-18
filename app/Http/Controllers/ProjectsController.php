e6288efc901799769c4b8e81948c726ecda2c7f3
saiki yasunori <hohner270@saiki-no-MacBook-Pro.local>
2018-11-18 15:14:59 +0900

    [update]

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -4,7 +4,52 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Project;
+
 class ProjectsController extends Controller
 {
-    //
+    private $project;
+
+    public function __construct(Project $project)
+    {
+        $this->project = $project;
+    }
+
+    public function index()
+    {
+        $projects = $this->project->all();
+
+        $data = [
+            'projects' => $projects,
+        ];
+
+        return response()->json($data);
+    }
+
+    public function create()
+    {
+        $data = [
+
+        ];
+
+        return response()->json($data);
+    }
+
+    public function update()
+    {
+        $data = [
+
+        ];
+
+        return response()->json($data);
+    }
+
+    public function delete()
+    {
+        $data = [
+
+        ];
+
+        return response()->json($data);
+    }
 }
